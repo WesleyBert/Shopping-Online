06e5ee759fe66cd8251e028753788fe07506aeb0
wesleyberto <wesleyberto4@gmail.com>
2024-05-20 11:26:22 -0300

    Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <main className="footerText">
+      <h1>Wesley Berto © Todos os direitos reservados</h1>
+    </main>
+  );
+}
